1291cbd8a745f1a6c5ed95cee8842f7dfe10fdcf
mayankchugh-learning <mayankchugh.learning@gmail.com>
2024-04-11 21:03:08 +0800

    AWS s3 operations code

---
+++ test.py
@@ -0,0 +1,10 @@
+import os
+
+# write a function to add two numbers and print the result
+def addition(num1, num2):
+    return num1 + num2
+
+
+print("The sum of 10 & 20 is: ", addition(10, 20))
+
+print("The sum of 10 & 20 is: ", addition(5, 23))
